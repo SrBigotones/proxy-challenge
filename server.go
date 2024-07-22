100a472568b50dee7a3466becd955862676f9f4f
Augusto Gangui <augustogangui@gmail.com>
2024-07-22 00:56:58 -0300

    redis with middleware

--- server.go
+++ server.go
@@ -1,25 +1,110 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"strings"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/redis/go-redis/v9"
 )
 
-const limitPerIP = 1000
-const limitCat = 10_000
+const limitPerIP = 2 //1000
+const limitCat = 3   //10_000
 const limitItems = 10
 
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("GET /*", doThings)
+var client = redis.NewClient(&redis.Options{
+	Addr:     "localhost:6379",
+	Password: "",
+	DB:       0,
+})
+var ctx = context.Background()
 
-	http.ListenAndServe(":8080", mux)
+func main() {
+
+	r := mux.NewRouter()
+
+	r.Use(middleware)
+
+	// r.HandleFunc("/", getRequest)
+
+	r.PathPrefix("/categories").HandlerFunc(getCategories)
+
+	r.PathPrefix("/items").HandlerFunc(getItem)
+
+	http.ListenAndServe(":8080", r)
 }
 
-func doThings(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("New connection")
+func middleware(next http.Handler) http.Handler {
+	fmt.Println("Request Middleware")
 
-	response := fmt.Sprintf("New request from %s", r.RemoteAddr)
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		currentMinute := time.Now().Minute()
 
-	fmt.Fprintf(w, response)
+		redisKey := fmt.Sprintf("%s:%d", strings.Split(r.RemoteAddr, ":")[0], currentMinute)
+
+		reqCheck, err := client.Incr(ctx, redisKey).Result()
+
+		fmt.Println(reqCheck)
+		if err != nil {
+			panic(err)
+		}
+
+		if reqCheck > limitPerIP {
+			fmt.Fprintf(w, "Alcanzo el limite de request, espere un minuto")
+		} else {
+			next.ServeHTTP(w, r)
+		}
+
+	})
+}
+
+/*
+*
+ipSolicitante:NumMinuto -> Request count
+*/
+
+func getCategories(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Request Categories")
+	fmt.Println(r.RemoteAddr)
+	currentMinute := time.Now().Minute()
+
+	redisKey := fmt.Sprintf("categories:%d", currentMinute)
+
+	reqCheck, err := client.Incr(ctx, redisKey).Result()
+
+	fmt.Println(reqCheck)
+	if err != nil {
+		panic(err)
+	}
+
+	if reqCheck > limitCat {
+		fmt.Fprintf(w, "Alcanzo el limite de request, espere un minuto")
+	} else {
+		fmt.Fprintf(w, "Your content: herererere")
+	}
+}
+
+func getItem(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("Request Item")
+	currentMinute := time.Now().Minute()
+
+	redisKey := fmt.Sprintf("%s/items:%d", strings.Split(r.RemoteAddr, ":")[0], currentMinute)
+
+	reqCheck, err := client.Incr(ctx, redisKey).Result()
+
+	fmt.Println(reqCheck)
+	if err != nil {
+		panic(err)
+	}
+
+	if reqCheck > limitItems {
+		fmt.Fprintf(w, "Alcanzo el limite de request, espere un minuto")
+	} else {
+		fmt.Fprintf(w, "Your content: herererere")
+	}
+
 }
