28c1e42b9ae53ff594e5d03c64186798b6e0532b
Augusto Gangui <augustogangui@gmail.com>
2024-07-22 03:23:09 -0300

    primitive logger

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -16,6 +17,7 @@ const limitPerIP = 2 //1000
 const limitCat = 1   //10_000
 const limitItems = 10
 const API_URL = "https://api.mercadolibre.com"
+const logStats = true
 
 var client = redis.NewClient(&redis.Options{
 	Addr:     "localhost:6379",
@@ -42,17 +44,26 @@ ipSolicitante:NumMinuto -> Request count
 func middleware(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		startTime := time.Now().UnixMilli()
+		lrw := NewLogginResponseWrite(w, r)
 
 		fmt.Printf("Request from %s to %s\n", r.RemoteAddr, r.URL)
 
 		reqCheck := readContraintValue(strings.Split(r.RemoteAddr, ":")[0], limitPerIP)
 
 		if !reqCheck {
+			w.WriteHeader(http.StatusTooManyRequests)
 			fmt.Fprintf(w, "Alcanzo el limite de request, espere un minuto")
 		} else {
-			next.ServeHTTP(w, r)
+			next.ServeHTTP(lrw, r)
 		}
 
+		if logStats {
+			endTime := time.Now().UnixMilli() - startTime
+			lrw.ResponseTimeMs = endTime
+			stat2D, _ := json.Marshal(lrw)
+			fmt.Println(string(stat2D))
+		}
 	})
 }
 
@@ -61,6 +72,7 @@ func getCategories(w http.ResponseWriter, r *http.Request) {
 	reqCheck := readContraintValue("categories", limitCat)
 
 	if !reqCheck {
+		w.WriteHeader(http.StatusTooManyRequests)
 		fmt.Fprintf(w, "Alcanzo el limite de request, espere un minuto")
 	} else {
 		getContentFromAPI(r.URL.Path, w)
@@ -72,6 +84,7 @@ func getItem(w http.ResponseWriter, r *http.Request) {
 	reqCheck := readContraintValue("items", limitItems)
 
 	if !reqCheck {
+		w.WriteHeader(http.StatusTooManyRequests)
 		fmt.Fprintf(w, "Alcanzo el limite de request, espere un minuto")
 	} else {
 		getContentFromAPI(r.URL.Path, w)
@@ -98,6 +111,7 @@ func getContentFromAPI(path string, w http.ResponseWriter) {
 		panic(err)
 	} else {
 		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(res.StatusCode)
 		resBody, err := io.ReadAll(res.Body)
 		if err != nil {
 			panic(err)
@@ -105,4 +119,22 @@ func getContentFromAPI(path string, w http.ResponseWriter) {
 
 		fmt.Fprint(w, string(resBody))
 	}
+}
+
+type ClientStat struct {
+	Ip                  string `json:"ip"`
+	Path                string `json:"path"`
+	ResponseTimeMs      int64  `json:"responseTimeMs"`
+	StatusCode          int    `json:"statusCode"`
+	http.ResponseWriter `json:"-"`
+}
+
+func (stat *ClientStat) WriteHeader(code int) {
+	stat.StatusCode = code
+	stat.ResponseWriter.WriteHeader(code)
+}
+
+func NewLogginResponseWrite(w http.ResponseWriter, r *http.Request) *ClientStat {
+
+	return &ClientStat{ResponseWriter: w, StatusCode: 200, Ip: r.RemoteAddr, Path: r.URL.Path, ResponseTimeMs: 0}
 }
